8f0390789a4d2bdc6f60017cea18af84452a1287
AlrightErin27 <erinmontybruce@gmail.com>
2021-03-09 14:16:48 -0800

    about 3/4's done

---
+++ src/components/mood-note/MoodNote.jsx
@@ -0,0 +1,14 @@
+//Set Up//
+import { Component } from "react";
+export default class MoodNote extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          My mood was {this.props.points} on {this.props.date}
+        </p>
+        <p>My thoughts: {this.props.note}</p>
+      </div>
+    );
+  }
+}
--- src/components/mood-tracker/MoodTracker.jsx
+++ src/components/mood-tracker/MoodTracker.jsx
@@ -1,21 +1,16 @@
 //Class Based Components//
 import { Component } from "react";
 import MoodPoints from "../mood-points/MoodPoints";
+import MoodNote from "../mood-note/MoodNote";
+import placeholderNotes from "./placeholderNotes";
 
 export default class MoodTracker extends Component {
-  //   //constructor// this is the BOOMER WAY
-  //   constructor(props) {
-  //     //invoke super first!
-  //     //get functionality from Component
-  //     super(props);
-  //     this.state = {
-  //       points: 10,
-  //     };
-  //   }
-
+  //constructor//
   //class field declaration method of defining state
   state = {
-    points: 11,
+    points: 10,
+    noteInput: "",
+    noteData: [],
   };
 
   //events handlers//
@@ -39,15 +34,60 @@ export default class MoodTracker extends Component {
       () => console.log(this.state)
     );
   };
-
+  handleInputChange = (e) => {
+    this.setState(
+      {
+        noteInput: e.target.value,
+      },
+      () => console.log(this.state)
+    );
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState((prevState) => {
+      const newNoteData = {
+        note: prevState.noteInput,
+        date: new Date().toLocaleDateString(),
+        points: prevState.points,
+      };
+      return {
+        // noteData: prevState.noteData.push(newNoteData),/////DO NOT USE PUSH HERE!!!
+        //USE STRING CONCATENATION!!!//
+        // noteData: prevState.noteData.concat([newNoteData]),
+        //USE SPREAD OPERATOR//
+        noteData: [...prevState.noteData, newNoteData],
+      };
+    });
+  };
   //render//
   render() {
+    const noteComponents = this.state.noteData.map((placeholderNote, index) => {
+      return (
+        <MoodNote
+          key={`note ${index}`}
+          points={placeholderNote.points}
+          date={placeholderNote.date}
+          note={placeholderNote.note}
+        />
+      );
+    });
     return (
       <div>
         <MoodPoints points={this.state.points} />
-
-        <button onClick={this.handleIncreaseMood}>😁</button>
+        <button onClick={this.handleIncreaseMood}>😁</button>{" "}
         <button onClick={this.handleDecreaseMood}>☹️</button>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="note-input">New Note:</label>
+          <input
+            id="note-input"
+            type="text"
+            placeholder="how are you feeling?"
+            onChange={this.handleInputChange}
+          />
+          <input type="submit" value="Save Note" />
+        </form>
+        <h3>My Notes:</h3>
+        {noteComponents}
       </div>
     );
   }
